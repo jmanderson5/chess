c968483c564c5e7641ee4a4b5c0aaa80a557c2b0
Benjamin Anderson <benjaminanderson@Benjamins-MacBook-Air-90.local>
2025-01-24 02:12:20 -0700

    changed formatting

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,7 +7,9 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+
     private ChessPiece[][] squares = new ChessPiece[8][8];
+
     public ChessBoard() {
         
     }
