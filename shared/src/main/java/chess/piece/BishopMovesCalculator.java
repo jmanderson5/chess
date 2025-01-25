1377906910d865d739cd42f4a03130cedf49dbe4
Benjamin Anderson <benjaminanderson@BenjaminsAir90.app.byu.edu>
2025-01-25 11:36:11 -0700

    Implemented BishopMovesCalculator.java and PieceMovesCalculator.java classes

--- shared/src/main/java/chess/piece/BishopMovesCalculator.java
+++ shared/src/main/java/chess/piece/BishopMovesCalculator.java
@@ -1,4 +1,45 @@
 package chess.piece;
 
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.List;
+
 public class BishopMovesCalculator {
+
+    private List<ChessMove> legalMoves;
+
+    public List<ChessMove> getLegalMoves() {
+        return legalMoves;
+    }
+
+    public void setLegalMoves(List<ChessMove> legalMoves) {
+        this.legalMoves = legalMoves;
+    }
+
+    public BishopMovesCalculator(ChessBoard board, ChessPosition myPosition) {
+        int column = myPosition.getColumn();
+        int row = myPosition.getRow();
+
+        // top right
+        for (int i = column + 1, j = row + 1; i <= 8 && j <= 8; i++, j++) {
+            legalMoves.add(0, new ChessMove(new ChessPosition(i, j), myPosition, ChessPiece.PieceType.BISHOP));
+        }
+        // top left
+        for (int i = column + 1, j = row - 1; i <= 8 && j >= 0; i++, j--) {
+            legalMoves.add(0, new ChessMove(new ChessPosition(i, j), myPosition, ChessPiece.PieceType.BISHOP));
+        }
+        // bottom right
+        for (int i = column - 1, j = row + 1; i >= 0 && j <= 8; i--, j++) {
+            legalMoves.add(0, new ChessMove(new ChessPosition(i, j), myPosition, ChessPiece.PieceType.BISHOP));
+        }
+        // bottom left
+        for (int i = column - 1, j = row - 1; i >= 0 && j >= 0; i--, j--) {
+            legalMoves.add(0, new ChessMove(new ChessPosition(i, j), myPosition, ChessPiece.PieceType.BISHOP));
+        }
+
+        setLegalMoves(legalMoves);
+    }
 }
--- shared/src/main/java/chess/piece/PieceMovesCalculator.java
+++ shared/src/main/java/chess/piece/PieceMovesCalculator.java
@@ -1,4 +1,47 @@
 package chess.piece;
 
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.List;
+
+import static chess.ChessPiece.PieceType.BISHOP;
+
 public class PieceMovesCalculator {
+
+    private List<ChessMove> legalMoves;
+    private ChessBoard board;
+    private ChessPosition myPosition;
+
+    public List<ChessMove> getLegalMoves() {
+        return legalMoves;
+    }
+
+    private void setLegalMoves(List<ChessMove> legalMoves) {
+        this.legalMoves = legalMoves;
+    }
+
+    private void setBoard(ChessBoard board) {
+        this.board = board;
+    }
+
+    private void setMyPosition(ChessPosition myPosition) {
+        this.myPosition = myPosition;
+    }
+
+    private void calculateLegalMoves() {
+        ChessPiece piece = ChessBoard.getPiece(myPosition);
+        if (piece.getPieceType() == BISHOP) {
+            BishopMovesCalculator bishopMoves = new BishopMovesCalculator(board, myPosition);
+            setLegalMoves(bishopMoves.getLegalMoves());
+        }
+    }
+
+    public PieceMovesCalculator(ChessBoard board, ChessPosition myPosition) {
+        setBoard(board);
+        setMyPosition(myPosition);
+        calculateLegalMoves();
+    }
 }
