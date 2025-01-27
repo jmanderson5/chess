9432e4ba860791d5f7de80d096f92266d45d22ad
Benjamin Anderson <benjaminanderson@BenjaminsAir90.s.cs.byu.edu>
2025-01-27 13:48:06 -0700

    create KingMovesCalcultor.java with getters and setters

--- shared/src/main/java/chess/piece/KingMovesCalculator.java
+++ shared/src/main/java/chess/piece/KingMovesCalculator.java
@@ -1,4 +1,25 @@
 package chess.piece;
 
+import chess.ChessBoard;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class KingMovesCalculator {
+
+    private List<ChessMove> legalMoves = new ArrayList<>();
+
+    public List<ChessMove> getLegalMoves() {
+        return legalMoves;
+    }
+
+    public void setLegalMoves(List<ChessMove> legalMoves) {
+        this.legalMoves = legalMoves;
+    }
+
+    public KingMovesCalculator(ChessBoard board, ChessPosition myPosition) {
+        // implement KingMovesCalculator
+    }
 }
