3969fa7a8bb54538794abe624b00095a5741b8f8
Benjamin Anderson <benjaminanderson@BenjaminsAir90.s.cs.byu.edu>
2025-01-27 14:22:16 -0700

    implemented KingMovesCalculator.java

--- shared/src/main/java/chess/piece/KingMovesCalculator.java
+++ shared/src/main/java/chess/piece/KingMovesCalculator.java
@@ -2,6 +2,7 @@ package chess.piece;
 
 import chess.ChessBoard;
 import chess.ChessMove;
+import chess.ChessPiece;
 import chess.ChessPosition;
 
 import java.util.ArrayList;
@@ -37,8 +38,40 @@ public class KingMovesCalculator {
         return Objects.hashCode(legalMoves);
     }
 
+    private void kingMovesModifier(int rowNext, int columnNext, ChessPiece startingPiece, ChessBoard board,
+                                   ChessPosition myPosition) {
+        if (rowNext <= 8 && rowNext >= 1 && columnNext <= 8 && columnNext >= 1){
+            ChessPiece temp = board.getPiece(new ChessPosition(rowNext, columnNext));
+            if (temp == null) {
+                legalMoves.add(new ChessMove(myPosition, new ChessPosition(rowNext, columnNext), null));
+            } else if (temp.getTeamColor() != startingPiece.getTeamColor()) {
+                legalMoves.add(new ChessMove(myPosition, new ChessPosition(rowNext, columnNext), null));
+            }
+        }
+    }
+
     public KingMovesCalculator(ChessBoard board, ChessPosition myPosition) {
-        // implement KingMovesCalculator
+        int row = myPosition.getRow();
+        int column = myPosition.getColumn();
+
+        ChessPiece startingPiece = board.getPiece(myPosition);
+
+        // up one
+        kingMovesModifier(row + 1, column, startingPiece, board, myPosition);
+        // topRight
+        kingMovesModifier(row + 1, column + 1, startingPiece, board, myPosition);
+        // topLeft
+        kingMovesModifier(row + 1, column - 1, startingPiece, board, myPosition);
+        // left
+        kingMovesModifier(row, column - 1, startingPiece, board, myPosition);
+        // right
+        kingMovesModifier(row, column + 1, startingPiece, board, myPosition);
+        // down one
+        kingMovesModifier(row - 1, column, startingPiece, board, myPosition);
+        // bottomRight
+        kingMovesModifier(row - 1, column + 1, startingPiece, board, myPosition);
+        // bottomLeft
+        kingMovesModifier(row - 1, column - 1, startingPiece, board, myPosition);
 
         setLegalMoves(legalMoves);
     }
