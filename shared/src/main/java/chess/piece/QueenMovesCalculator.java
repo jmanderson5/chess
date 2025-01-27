d2ea7a827c41e6d7d8c67c80fccd9c8bfb14e37b
Benjamin Anderson <benjaminanderson@BenjaminsAir90.s.cs.byu.edu>
2025-01-27 16:41:51 -0700

    implemented QueenMovesCalculator.java

--- shared/src/main/java/chess/piece/QueenMovesCalculator.java
+++ shared/src/main/java/chess/piece/QueenMovesCalculator.java
@@ -20,20 +20,68 @@ public class QueenMovesCalculator {
         this.legalMoves = legalMoves;
     }
 
-    private void queenMovesModifier(int rowNext, int columnNext, ChessPiece startingPiece, ChessBoard board,
-                                   ChessPosition myPosition) {
-        if (rowNext <= 8 && rowNext >= 1 && columnNext <= 8 && columnNext >= 1){
-            ChessPiece temp = board.getPiece(new ChessPosition(rowNext, columnNext));
-            if (temp == null) {
-                legalMoves.add(new ChessMove(myPosition, new ChessPosition(rowNext, columnNext), null));
-            } else if (temp.getTeamColor() != startingPiece.getTeamColor()) {
-                legalMoves.add(new ChessMove(myPosition, new ChessPosition(rowNext, columnNext), null));
-            }
-        }
-    }
-
     QueenMovesCalculator(ChessBoard board, ChessPosition myPosition) {
-        // implement QueenMovesCalculator
+        int row = myPosition.getRow();
+        int column = myPosition.getColumn();
+
+        ChessPiece startingPiece = board.getPiece(myPosition);
+
+        // up
+        for (int i = row + 1; i <= 8; i++) {
+            ChessPiece temp = board.getPiece(new ChessPosition(i, column));
+            if (temp != null && temp.getTeamColor() == startingPiece.getTeamColor()) { break; }
+            legalMoves.add(new ChessMove(myPosition, new ChessPosition(i, column), null));
+            if (temp != null) { break; }
+        }
+        // top right
+        for (int i = row + 1, j = column + 1; i <= 8 && j <= 8; i++, j++) {
+            ChessPiece temp = board.getPiece(new ChessPosition(i, j));
+            if (temp != null && temp.getTeamColor() == startingPiece.getTeamColor()) { break; }
+            legalMoves.add(new ChessMove(myPosition, new ChessPosition(i, j), null));
+            if (temp != null) { break; }
+        }
+        // right
+        for (int j = column + 1; j <= 8; j++) {
+            ChessPiece temp = board.getPiece(new ChessPosition(row, j));
+            if (temp != null && temp.getTeamColor() == startingPiece.getTeamColor()) { break; }
+            legalMoves.add(new ChessMove(myPosition, new ChessPosition(row, j), null));
+            if (temp != null) { break; }
+        }
+        // bottom right
+        for (int i = row - 1, j = column + 1; i >= 1 && j <= 8; i--, j++) {
+            ChessPiece temp = board.getPiece(new ChessPosition(i, j));
+            if (temp != null && temp.getTeamColor() == startingPiece.getTeamColor()) { break; }
+            legalMoves.add(new ChessMove(myPosition, new ChessPosition(i, j), null));
+            if (temp != null) { break; }
+        }
+        // down
+        for (int i = row - 1; i >= 1; i--) {
+            ChessPiece temp = board.getPiece(new ChessPosition(i, column));
+            if (temp != null && temp.getTeamColor() == startingPiece.getTeamColor()) { break; }
+            legalMoves.add(new ChessMove(myPosition, new ChessPosition(i, column), null));
+            if (temp != null) { break; }
+        }
+        // bottom left
+        for (int i = row - 1, j = column - 1; i >= 1 && j >= 1; i--, j--) {
+            ChessPiece temp = board.getPiece(new ChessPosition(i, j));
+            if (temp != null && temp.getTeamColor() == startingPiece.getTeamColor()) { break; }
+            legalMoves.add(new ChessMove(myPosition, new ChessPosition(i, j), null));
+            if (temp != null) { break; }
+        }
+        // left
+        for (int j = column - 1; j >= 1; j--) {
+            ChessPiece temp = board.getPiece(new ChessPosition(row, j));
+            if (temp != null && temp.getTeamColor() == startingPiece.getTeamColor()) { break; }
+            legalMoves.add(new ChessMove(myPosition, new ChessPosition(row, j), null));
+            if (temp != null) { break; }
+        }
+        // top left
+        for (int i = row + 1, j = column - 1; i <= 8 && j >= 1; i++, j--) {
+            ChessPiece temp = board.getPiece(new ChessPosition(i, j));
+            if (temp != null && temp.getTeamColor() == startingPiece.getTeamColor()) { break; }
+            legalMoves.add(new ChessMove(myPosition, new ChessPosition(i, j), null));
+            if (temp != null) { break; }
+        }
 
         setLegalMoves(legalMoves);
     }
