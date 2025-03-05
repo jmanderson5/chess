214b9ea97e060c119e9cbbca8da8b733920b309b
Benjamin Anderson <benjaminanderson@Benjamins-MacBook-Air-251.local>
2025-03-04 19:40:36 -0700

    Updated dataaccess files and created Register service class and test

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -22,4 +22,8 @@ public class Server {
         Spark.stop();
         Spark.awaitStop();
     }
+
+    private void register() {
+
+    }
 }
--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,5 +1,4 @@
 package model;
 
 public record AuthData(String authToken, String username) {
-
 }
