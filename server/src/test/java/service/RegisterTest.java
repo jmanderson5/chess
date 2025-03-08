fb0552c526d4811d4f550540f3656b0cbc229f7b
Benjamin Anderson <benjaminanderson@Benjamins-MacBook-Air-251.local>
2025-03-07 18:23:23 -0700

    Added a test for a successful output from Register.java

--- server/src/test/java/service/RegisterTest.java
+++ server/src/test/java/service/RegisterTest.java
@@ -1,5 +1,28 @@
 package service;
 
-public class RegisterTest {
+import dataaccess.DataAccessException;
+import model.AuthResponse;
+import model.UserData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RegisterTest {
+    static final Register service = new Register();
+
+//    @BeforeEach
+//    void clear() throws ResponseException {
+//        service.clear();
+//    }
+
+    @Test
+    void runRegister() throws DataAccessException {
+        UserData user = new UserData("jmander", "happy", "jmander@byu.edu");
+        AuthResponse data = service.runRegister(user);
+
+        assertEquals(data.username(), "jmander");
+        assertNotNull(data.authToken());
+        assertTrue(data.authToken().matches("[a-zA-Z0-9]{10,}"));
+    }
 }
