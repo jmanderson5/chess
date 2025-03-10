856bff1c03c166a3523abcc70a6ba647e676a5d6
Benjamin Anderson <benjaminanderson@Benjamins-MacBook-Air-251.local>
2025-03-10 15:06:22 -0600

    implemented JoinGameTest.java

--- server/src/test/java/service/JoinGameTest.java
+++ server/src/test/java/service/JoinGameTest.java
@@ -1,4 +1,49 @@
 package service;
 
+import chess.ChessGame;
+import dataaccess.*;
+import model.AuthData;
+import model.GameData;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class JoinGameTest {
+    private AuthDAO authDAO = new MemoryAuthDAO();
+    private GameDAO gameDAO = new MemoryGameDAO();
+    private JoinGame service = new JoinGame();
+
+    @BeforeEach
+    void setup() {
+        authDAO = new MemoryAuthDAO();
+        gameDAO = new MemoryGameDAO();
+    }
+
+    @Test
+    void joinGameValid() throws DataAccessException {
+        ChessGame game = new ChessGame();
+        authDAO.createAuth(new AuthData("1234", "jmander"));
+        gameDAO.createGame(new GameData(1234, "", "",
+                "gameName", game));
+        service.runJoinGame(authDAO, gameDAO, "1234", "WHITE", 1234);
+        GameData referenceGame = new GameData(1234, "jmander", "",
+                "gameName", game);
+
+        assertEquals(referenceGame, gameDAO.getGameByID(1234));
+    }
+
+    @Test
+    void notUnauthorized() throws DataAccessException {
+        ChessGame game = new ChessGame();
+        gameDAO.createGame(new GameData(1234, "", "",
+                "gameName", game));
+
+        Exception exception = assertThrows(DataAccessException.class, () -> {
+            service.runJoinGame(authDAO, gameDAO, "1234", "WHITE", 1234);
+        });
+
+        assertEquals("Error: unauthorized", exception.getMessage());
+    }
 }
