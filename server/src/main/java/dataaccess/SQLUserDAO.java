81d76e8b666899c7d8e3c9eda4689a672f13f01c
Benjamin Anderson <benjaminanderson@Benjamins-MacBook-Air-251.local>
2025-03-12 19:53:27 -0600

    created userData table

--- server/src/main/java/dataaccess/SQLUserDAO.java
+++ server/src/main/java/dataaccess/SQLUserDAO.java
@@ -3,6 +3,24 @@ package dataaccess;
 import model.UserData;
 
 public class SQLUserDAO implements UserDAO {
+    SQLFunctions calculator = new SQLFunctions();
+
+    public SQLUserDAO() throws DataAccessException {
+        String[] createStatements = {
+                """
+            CREATE TABLE IF NOT EXISTS  userData (
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`gameID`),
+              INDEX(gameName)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+        };
+        calculator.configureDatabase(createStatements);
+    }
+
     @Override
     public UserData getUser(String username) {
         return null;
