2df3374e7147bdca79a3a3a8618539a466202915
Benjamin Anderson <benjaminanderson@Benjamins-MacBook-Air-251.local>
2025-03-12 18:09:58 -0600

    Moved more functions to SQLFunctions.java

--- server/src/main/java/dataaccess/SQLAuthDAO.java
+++ server/src/main/java/dataaccess/SQLAuthDAO.java
@@ -11,7 +11,17 @@ public class SQLAuthDAO implements AuthDAO {
     SQLFunctions calculator = new SQLFunctions();
 
     public SQLAuthDAO() throws DataAccessException {
-        configureDatabase();
+        String[] createStatements = {
+                """
+            CREATE TABLE IF NOT EXISTS  authData (
+              `authToken` varchar(256) NOT NULL,
+              `username` varchar(256) NOT NULL,
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`authToken`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+        };
+        calculator.configureDatabase(createStatements);
     }
 
     @Override
@@ -22,7 +32,7 @@ public class SQLAuthDAO implements AuthDAO {
                 ps.setString(1, authToken);
                 try (var rs = ps.executeQuery()) {
                     if (rs.next()) {
-                        return readAuthData(rs);
+                        return calculator.readAuthData(rs);
                     }
                 }
             }
@@ -50,36 +60,5 @@ public class SQLAuthDAO implements AuthDAO {
     public void clearAuthData() throws DataAccessException {
         String statement = "TRUNCATE authData";
         calculator.executeUpdate(statement);
-    }
-
-    private AuthData readAuthData(ResultSet rs) throws SQLException {
-        String authToken = rs.getString("authToken");
-        var json = rs.getString("json");
-        AuthData authData = new Gson().fromJson(json, AuthData.class);
-        return authData.setAuth(authToken);
-    }
-
-    private final String[] createStatements = {
-            """
-            CREATE TABLE IF NOT EXISTS  authData (
-              `authToken` varchar(256) NOT NULL,
-              `username` varchar(256) NOT NULL,
-              `json` TEXT DEFAULT NULL,
-              PRIMARY KEY (`authToken`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
-            """
-    };
-
-    private void configureDatabase() throws DataAccessException {
-        DatabaseManager.createDatabase();
-        try (var conn = DatabaseManager.getConnection()) {
-            for (var statement : createStatements) {
-                try (var preparedStatement = conn.prepareStatement(statement)) {
-                    preparedStatement.executeUpdate();
-                }
-            }
-        } catch (SQLException ex) {
-            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
-        }
     }
 }
--- server/src/main/java/dataaccess/SQLFunctions.java
+++ server/src/main/java/dataaccess/SQLFunctions.java
@@ -1,11 +1,16 @@
 package dataaccess;
 
+import com.google.gson.Gson;
+import model.AuthData;
+
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import static java.sql.Statement.RETURN_GENERATED_KEYS;
 import static java.sql.Types.NULL;
 
 public class SQLFunctions {
+
     public void executeUpdate(String statement, Object... params) throws DataAccessException {
         try (var conn = DatabaseManager.getConnection()) {
             try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
@@ -20,5 +25,25 @@ public class SQLFunctions {
         } catch (SQLException e) {
             throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
         }
+    }
+
+    public void configureDatabase(String[] statements) throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : statements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+
+    public AuthData readAuthData(ResultSet rs) throws SQLException {
+        String authToken = rs.getString("authToken");
+        var json = rs.getString("json");
+        AuthData authData = new Gson().fromJson(json, AuthData.class);
+        return authData.setAuth(authToken);
     }
 }
