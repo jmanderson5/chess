8e7987791a2f0de0d2d9672e7d3ac7cd53942ca1
Benjamin Anderson <benjaminanderson@Benjamins-MacBook-Air-251.local>
2025-03-13 20:55:55 -0600

    wrote some UserDAO tests

--- server/src/test/java/dataaccess/UserDAOTest.java
+++ server/src/test/java/dataaccess/UserDAOTest.java
@@ -1,10 +1,15 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.GameData;
 import model.UserData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class UserDAOTest {
 
@@ -26,9 +31,26 @@ public class UserDAOTest {
     }
 
     @Test
+    void getUserFail() throws DataAccessException {
+        UserData userData = new UserData("bill", "password", "littleMan");
+        userDAO.createUser(userData);
+
+        assertNotEquals(userData, userDAO.getUser("bob"));
+    }
+
+    @Test
     void createUserTest() {
         UserData userData = new UserData("jmander", "password", "jmander@byu.edu");
         assertDoesNotThrow(() -> userDAO.createUser(userData));
+    }
+
+    @Test
+    void createUserFail() throws DataAccessException {
+        UserData userData = new UserData("jmander", "password", "jmander@byu.edu");
+        UserData wrongUser = new UserData("bill", "password", "billEmail");
+        assertDoesNotThrow(() -> userDAO.createUser(wrongUser));
+
+        assertNotEquals(userData, userDAO.getUser(wrongUser.username()));
     }
 
     @Test
