a727eb6450800ceb99f4cbc508635e703d50983d
Benjamin Anderson <benjaminanderson@Benjamins-MacBook-Air-251.local>
2025-03-26 13:06:55 -0600

    updated server facade

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,4 +1,97 @@
 package server;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import model.handler.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
 public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        this.serverUrl = url;
+    }
+
+    public Object login() throws DataAccessException {
+        return null;
+    }
+
+    public Object logout() throws DataAccessException {
+        return null;
+    }
+
+    public Object listGames() throws DataAccessException {
+        return null;
+    }
+
+    public Object createGame(ChessGame game) throws DataAccessException {
+        String path = "/game";
+        String method = "POST";
+        return this.makeRequest(method, path, game, ChessGame.class);
+    }
+
+    public Object joinGame() throws DataAccessException {
+        return null;
+    }
+
+    public Object clear() throws DataAccessException {
+        return null;
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass)
+            throws DataAccessException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (ResponseException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            try (InputStream respErr = http.getErrorStream()) {
+                if (respErr != null) {
+                    throw ResponseException.fromJson(respErr);
+                }
+            }
+
+            throw new ResponseException(status, "other failure: " + status);
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
 }
