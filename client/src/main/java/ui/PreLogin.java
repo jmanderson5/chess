d5957c6286dfbc05d1dea3cf668ef6d50d82f8f7
Benjamin Anderson <benjaminanderson@Benjamins-MacBook-Air-251.local>
2025-03-28 14:38:56 -0600

    implemented register and login

--- client/src/main/java/ui/PreLogin.java
+++ client/src/main/java/ui/PreLogin.java
@@ -1,25 +1,68 @@
 package ui;
 
+import exception.ResponseException;
 import model.UserData;
+import model.handler.LoginData;
 import server.ServerFacade;
 
 public class PreLogin {
+
+    private ServerFacade serverFacade;
+
     public boolean run(String input, ServerFacade serverFacade) {
+        this.serverFacade = serverFacade;
+        boolean loggedIn = false;
+
         String[] parts = input.split(" ");
-        if (parts[0].equals("register")) { register(parts[1], parts[2], parts[3]); }
-        if (parts[0].equals("login")) { login(parts[1], parts[2]); }
+        if (parts[0].equals("register")) { loggedIn = register(parts[1], parts[2], parts[3]); }
+        if (parts[0].equals("login")) { loggedIn = login(parts[1], parts[2]); }
         if (parts[0].equals("help")) { help(); }
 
-        return true;
+        return loggedIn;
     }
 
-    private void register(String username, String password, String email) {
+    private boolean register(String username, String password, String email) {
         UserData user = new UserData(username, password, email);
+        boolean registered = false;
 
+        try {
+            serverFacade.register(user);
+            registered = true;
+        } catch (ResponseException e) {
+            System.out.print(EscapeSequences.SET_TEXT_COLOR_MAGENTA);
+            System.out.println(e.getMessage());
+            System.out.print(EscapeSequences.RESET_TEXT_COLOR);
+        }
+
+        if (registered) {
+            System.out.print(EscapeSequences.SET_TEXT_COLOR_MAGENTA);
+            System.out.print("Successfully registered");
+            System.out.println(EscapeSequences.RESET_TEXT_COLOR);
+            return registered;
+        }
+        return registered;
     }
 
-    private void login(String username, String password) {
+    private boolean login(String username, String password) {
+        LoginData loginData = new LoginData(username, password);
+        boolean loggedIn = false;
 
+        try {
+            serverFacade.login(loginData);
+            loggedIn = true;
+        } catch (ResponseException e) {
+            System.out.print(EscapeSequences.SET_TEXT_COLOR_MAGENTA);
+            System.out.println(e.getMessage());
+            System.out.print(EscapeSequences.RESET_TEXT_COLOR);
+        }
+
+        if (loggedIn) {
+            System.out.print(EscapeSequences.SET_TEXT_COLOR_MAGENTA);
+            System.out.print("Successful login");
+            System.out.println(EscapeSequences.RESET_TEXT_COLOR);
+            return loggedIn;
+        }
+        return loggedIn;
     }
 
     private void help() {
