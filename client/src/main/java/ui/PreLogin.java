307bfa5a12b61fce186f23f88d4fe522f0826391
Benjamin Anderson <benjaminanderson@Benjamins-MacBook-Air-251.local>
2025-03-28 13:45:56 -0600

    wrote help code for pre login

--- client/src/main/java/ui/PreLogin.java
+++ client/src/main/java/ui/PreLogin.java
@@ -2,6 +2,23 @@ package ui;
 
 public class PreLogin {
     public boolean run(String input) {
-        return true;
+        if (input.equals("help")) { help(); }
+
+        return false;
+    }
+
+    private void help() {
+        writeHelpText("register <USERNAME> <PASSWORD> <EMAIL>", " - to create an account");
+        writeHelpText("login <USERNAME> <PASSWORD>", " - to play chess");
+        writeHelpText("quit", " - playing chess");
+        writeHelpText("help", " - with possible commands");
+        System.out.println(EscapeSequences.RESET_TEXT_COLOR);
+    }
+
+    private void writeHelpText(String command, String description) {
+        System.out.print(EscapeSequences.SET_TEXT_COLOR_MAGENTA);
+        System.out.print(command);
+        System.out.print(EscapeSequences.SET_TEXT_COLOR_WHITE);
+        System.out.println(description);
     }
 }
