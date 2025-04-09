56acfc2689b37bb0762058347b5161953e55dc57
Benjamin Anderson <benjaminanderson@Benjamins-MacBook-Air-251.local>
2025-04-09 13:58:28 -0600

    created separate Websocket and Http communicator classes

--- client/src/main/java/server/ServerFacade.java
+++ client/src/main/java/server/ServerFacade.java
@@ -17,109 +17,55 @@ public class ServerFacade {
 
     private final String serverUrl;
     private String authToken;
+    private HttpCommunicator httpCommunicator;
+    private WebsocketCommunicator websocketCommunicator;
 
     public ServerFacade(String url) {
         this.serverUrl = url;
+        httpCommunicator = new HttpCommunicator(serverUrl);
+        websocketCommunicator = new WebsocketCommunicator(serverUrl);
     }
 
     public AuthResponse register(UserData user) throws ResponseException {
         var path = "/user";
-        AuthResponse authResponse = this.makeRequest("POST", path, user, AuthResponse.class);
+        AuthResponse authResponse = httpCommunicator.makeRequest("POST", path, user, AuthResponse.class);
         authToken = authResponse.authToken();
+        httpCommunicator.setAuthToken(authToken);
         return authResponse;
     }
 
     public AuthResponse login(LoginData user) throws ResponseException {
         var path = "/session";
-        AuthResponse authResponse = this.makeRequest("POST", path, user, AuthResponse.class);
+        AuthResponse authResponse = httpCommunicator.makeRequest("POST", path, user, AuthResponse.class);
         authToken = authResponse.authToken();
+        httpCommunicator.setAuthToken(authToken);
         return authResponse;
     }
 
     public void logout() throws ResponseException {
         var path = "/session";
-        this.makeRequest("DELETE", path, null, null);
+        httpCommunicator.makeRequest("DELETE", path, null, null);
     }
 
     public Games listGames() throws ResponseException {
         var path = "/game";
-        return this.makeRequest("GET", path, null, Games.class);
+        return httpCommunicator.makeRequest("GET", path, null, Games.class);
     }
 
     public GameResult createGame(String game) throws ResponseException {
         String path = "/game";
         record GameName(String gameName) {}
-        return this.makeRequest("POST", path, new GameName(game), GameResult.class);
+        return httpCommunicator.makeRequest("POST", path, new GameName(game), GameResult.class);
     }
 
     public void joinGame(String userColor, Integer gameID) throws ResponseException {
         var path = "/game";
-        this.makeRequest("PUT", path, new JoinGameData(userColor, gameID), null);
+        httpCommunicator.makeRequest("PUT", path, new JoinGameData(userColor, gameID), null);
     }
 
     public void clear() throws ResponseException {
         var path = "/db";
-        this.makeRequest("DELETE", path, null, null);
+        httpCommunicator.makeRequest("DELETE", path, null, null);
         authToken = null;
-    }
-
-    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass)
-            throws ResponseException{
-        try {
-            URL url = (new URI(serverUrl + path)).toURL();
-            HttpURLConnection http = (HttpURLConnection) url.openConnection();
-            http.setRequestMethod(method);
-            http.addRequestProperty("authorization", authToken);
-            http.setDoOutput(true);
-
-            writeBody(request, http);
-            http.connect();
-            throwIfNotSuccessful(http);
-            return readBody(http, responseClass);
-        } catch (ResponseException ex) {
-            throw ex;
-        } catch (Exception ex) {
-            throw new ResponseException(500, ex.getMessage());
-        }
-    }
-
-    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
-        if (request != null) {
-            http.addRequestProperty("Content-Type", "application/json");
-            String reqData = new Gson().toJson(request);
-            try (OutputStream reqBody = http.getOutputStream()) {
-                reqBody.write(reqData.getBytes());
-            }
-        }
-    }
-
-    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
-        var status = http.getResponseCode();
-        if (!isSuccessful(status)) {
-            try (InputStream respErr = http.getErrorStream()) {
-                if (respErr != null) {
-                    throw ResponseException.fromJson(respErr);
-                }
-            }
-
-            throw new ResponseException(status, "other failure: " + status);
-        }
-    }
-
-    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
-        T response = null;
-        if (http.getContentLength() < 0) {
-            try (InputStream respBody = http.getInputStream()) {
-                InputStreamReader reader = new InputStreamReader(respBody);
-                if (responseClass != null) {
-                    response = new Gson().fromJson(reader, responseClass);
-                }
-            }
-        }
-        return response;
-    }
-
-    private boolean isSuccessful(int status) {
-        return status / 100 == 2;
     }
 }
---
+++ client/src/main/java/server/WebsocketCommunicator.java
@@ -0,0 +1,10 @@
+package server;
+
+public class WebsocketCommunicator {
+    String serverUrl;
+    String authToken;
+
+    public WebsocketCommunicator(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+}
